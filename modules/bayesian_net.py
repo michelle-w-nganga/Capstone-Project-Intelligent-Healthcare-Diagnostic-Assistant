d9bbbe0478bb62e9faacff0310b153ffbc7056f1
patrickcodes001 <patrickcodes58@gmail.com>
2026-08-03 12:00:00 +0300

    Refine Bayesian network

--- modules/bayesian_net.py
+++ modules/bayesian_net.py
@@ -143,4 +143,4 @@ if __name__ == "__main__":
     print("\nExplanation for covid19:")
     print("  " + bn.explain("covid19", test_symptoms))
 
-    print("\n[✔] Bayesian Network test successful!")
+    print("\n[✔] Bayesian Network test succeeded!")
